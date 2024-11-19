c3ac489a57dd89396865e5566b032cc3a8700d32
Rejohn-Glitch <jaranilla.rejohn@sfxc.edu.ph>
2024-11-19 17:55:47 +0800

    Add files via upload

---
+++ beginner.js
@@ -0,0 +1,9 @@
+let firstName = "John";
+let favorideFood = "Grapes";
+let email = "Bro@gmail.com"
+let online = true;
+
+console.log(`Are you ${online}`);
+console.log(`My email is ${email}`);
+console.log(`My favorite food is ${favorideFood}`);
+console.log(`Your first name is ${firstName}`);
